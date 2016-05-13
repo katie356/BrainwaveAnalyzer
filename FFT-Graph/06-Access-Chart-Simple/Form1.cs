107c7a2eb6d59c96b389c21b63f3b85d21dabed7
katie356 <Katie356333@gmail.com>
2016-05-12 23:52:09 -0700

    Add files via upload

--- FFT-Graph/06-Access-Chart-Simple/Form1.cs
+++ FFT-Graph/06-Access-Chart-Simple/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -446,7 +447,9 @@ namespace _06_Access_Chart_Simple
                 real[intLoop] = Convert.ToDouble(objA);
             }
 
-            Fft(real, imag);
+            //Fft myFFT = new Fft();
+
+            TransformRadix2(real, imag);
 
             for (int i = 0; i < intCount; i++)
             {
@@ -890,6 +893,86 @@ namespace _06_Access_Chart_Simple
             MessageBox.Show("Done Calculating Frequency");
 
         }
+
+        /*---- FFT implementation by Project Nayuki, https://www.nayuki.io/page/free-small-fft-in-multiple-languages ----*/
+
+        private static void TransformRadix2(double[] real, double[] imag)
+        {
+            // Initialization
+            if (real.Length != imag.Length)
+                throw new System.ArgumentException("Mismatched lengths");
+            int n = real.Length;
+            int levels = 31 - NumberOfLeadingZeros(n);  // Equal to floor(log2(n))
+            if (1 << levels != n)
+                throw new System.ArgumentException("Length is not a power of 2");
+            double[] cosTable = new double[n / 2];
+            double[] sinTable = new double[n / 2];
+            for (int i = 0; i < n / 2; i++)
+            {
+                cosTable[i] = System.Math.Cos(2 * System.Math.PI * i / n);
+                sinTable[i] = System.Math.Sin(2 * System.Math.PI * i / n);
+            }
+
+            // Bit-reversed addressing permutation
+            for (int i = 0; i < n; i++)
+            {
+                int j = (int)((uint)ReverseBits(i) >> (32 - levels));  //int j = ReverseBits(i) >> (32 - levels);
+                if (j > i)
+                {
+                    double temp = real[i];
+                    real[i] = real[j];
+                    real[j] = temp;
+                    temp = imag[i];
+                    imag[i] = imag[j];
+                    imag[j] = temp;
+                }
+            }
+
+            // Cooley-Tukey decimation-in-time radix-2 FFT
+            for (int size = 2; size <= n; size *= 2)
+            {
+                int halfsize = size / 2;
+                int tablestep = n / size;
+                for (int i = 0; i < n; i += size)
+                {
+                    for (int j = i, k = 0; j < i + halfsize; j++, k += tablestep)
+                    {
+                        double tpre = real[j + halfsize] * cosTable[k] + imag[j + halfsize] * sinTable[k];
+                        double tpim = -real[j + halfsize] * sinTable[k] + imag[j + halfsize] * cosTable[k];
+                        real[j + halfsize] = real[j] - tpre;
+                        imag[j + halfsize] = imag[j] - tpim;
+                        real[j] += tpre;
+                        imag[j] += tpim;
+                    }
+                }
+                if (size == n)  // Prevent overflow in 'size *= 2'
+                    break;
+            }
+        }
+
+        private static int NumberOfLeadingZeros(int val)
+        {
+            if (val == 0)
+                return 32;
+            int result = 0;
+            while (val >= 0)
+            {
+                val <<= 1;
+                result++;
+            }
+            return result;
+        }
+
+        private static int ReverseBits(int val)
+        {
+            int result = 0;
+            for (int i = 0; i < 32; i++, val >>= 1)
+                result = (result << 1) | (val & 1);
+            return result;
+        }
+
+
+
 
     }
     
