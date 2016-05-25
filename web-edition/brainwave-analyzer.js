e6bd8b5390514cae399931876c7d047e8e8f32fa
Project Nayuki <me@nayuki.io>
2016-05-25 18:28:59 +0000

    Split the Brainwave Analyzer web version into multiple source files for ease of maintenance, without changing functionality.

---
+++ web-edition/brainwave-analyzer.js
@@ -0,0 +1,133 @@
+"use strict";
+
+
+function doClear() {
+	document.getElementById("input-text").value = "";
+	removeAllChildren(document.getElementById("results"));
+	inputTextElem.className = "";
+}
+
+
+function doAnalyze() {
+	var inputTextElem = document.getElementById("input-text");
+	var text = inputTextElem.value;
+	text = text.replace(/\n+$/g, "");  // Strip trailing newlines
+	var lines = text.split("\n");
+	
+	var header = lines[0].split(",");
+	var electrodeColIndex = -1;
+	for (var i = 0; i < header.length; i++) {
+		if (header[i] == "Electrode") {
+			if (electrodeColIndex != -1) {
+				alert("Error: Duplicate column \"Electrode\"");
+				return;
+			} else {
+				electrodeColIndex = i;
+			}
+		}
+	}
+	
+	var skippedRows = 0;
+	var invalidValues = 0;
+	var samples = [];
+	for (var i = 1; i < lines.length; i++) {
+		var columns = lines[i].split(",");
+		if (columns.length != header.length) {
+			skippedRows++;
+		} else {
+			var strValue = columns[electrodeColIndex];
+			var numValue;
+			if (/^[+-]?\d+(?:\.\d*)?$/.test(strValue))
+				numValue = parseFloat(strValue)
+			else {
+				invalidValues++;
+				numValue = 0;
+			}
+			samples.push(numValue);
+		}
+	}
+	if (skippedRows > 0)
+		alert("Warning: Skipped " + skippedRows + " rows in the input data due to invalid format");
+	if (invalidValues > 0)
+		alert("Warning: Replaced " + invalidValues + " invalid electrode values in the input data");
+	inputTextElem.className = "minimize";
+	
+	setTimeout(function() {
+		doComputation(samples);
+	}, 250);
+}
+
+
+var SAMPLES_PER_SECOND = 512;
+
+function doComputation(samples) {
+	var resultsElem = document.getElementById("results");
+	removeAllChildren(resultsElem);
+	
+	var numSeconds = Math.floor(samples.length / SAMPLES_PER_SECOND);
+	for (var i = 0; i < numSeconds; i++) {
+		var startIndex = (i + 0) * SAMPLES_PER_SECOND;  // Inclusive
+		var endIndex   = (i + 1) * SAMPLES_PER_SECOND;  // Exclusive
+		var block = samples.slice(startIndex, endIndex)
+		var real = block.slice();  // Clone
+		var imag = real.map(function() { return 0; });  // Same length, but all zeros
+		transform(real, imag);  // FFT
+		real[0] = 0;  // Cancel the DC offset coefficient
+		var amplitude = [];
+		for (var j = 0; j <= real.length / 2; j++)
+			amplitude.push(Math.hypot(real[j], imag[j]));
+		var maxAmplitude = Math.max.apply(null, amplitude);
+		
+		var h3 = document.createElement("h3");
+		h3.appendChild(document.createTextNode("Time = " + i + " seconds"));
+		resultsElem.appendChild(h3);
+		
+		var p = document.createElement("p");
+		p.appendChild(document.createTextNode("Brainwave:"));
+		resultsElem.appendChild(p);
+		
+		var graphElem = document.createElement("div");
+		graphElem.className = "wavegraph";
+		var minBlock = Math.min.apply(null, block);
+		var maxBlock = Math.max.apply(null, block);
+		for (var j = 0; j < block.length; j++) {
+			var fullHeight = 4;
+			var dotOffset = (maxBlock - block[j]) / (maxBlock - minBlock) * fullHeight;
+			var dotElem = document.createElement("div");
+			dotElem.style.top = "calc(" + dotOffset.toFixed(3) + "em - 1px)";
+			dotElem.title = (i + j / SAMPLES_PER_SECOND).toFixed(3) + " s";
+			graphElem.appendChild(dotElem);
+		}
+		resultsElem.appendChild(graphElem);
+		
+		p = document.createElement("p");
+		p.appendChild(document.createTextNode("Frequency spectrum:"));
+		resultsElem.appendChild(p);
+		
+		graphElem = document.createElement("div");
+		graphElem.className = "freqgraph";
+		for (var j = 0; j <= real.length / 2; j++) {
+			var fullHeight = 10;
+			var barHeight = amplitude[j] / maxAmplitude * fullHeight;
+			var barElem = document.createElement("div");
+			barElem.style.height = barHeight.toFixed(3) + "em";
+			barElem.title = j + " Hz";
+			graphElem.appendChild(barElem);
+		}
+		resultsElem.appendChild(graphElem);
+	}
+}
+
+
+function removeAllChildren(node) {
+	while (node.firstChild != null)
+		node.removeChild(node.firstChild);
+}
+
+
+// Polyfill
+if (!("hypot" in Math)) {
+	Math.hypot = function(x, y) {
+		return Math.sqrt(x * x + y * y);
+	};
+}
