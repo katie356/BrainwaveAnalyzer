9de7c1300039af030e55aa120d233d879113055a
Project Nayuki <me@nayuki.io>
2016-06-08 06:09:52 +0000

    Inlined the single FFT JavaScript function into the main JavaScript code.

--- web-edition/resources/brainwave-analyzer.js
+++ web-edition/resources/brainwave-analyzer.js
@@ -146,7 +146,7 @@ function computeAndAnalyze(samples) {
 		
 		var real = block.slice();  // Clone
 		var imag = real.map(function() { return 0; });  // Same length, but all zeros
-		transform(real, imag);  // FFT
+		fastFourierTransform(real, imag);
 		real[0] = 0;  // Cancel the DC offset coefficient
 		
 		var amplitude = [];
@@ -383,4 +383,98 @@ if (!("hypot" in Math)) {
 	Math.hypot = function(x, y) {
 		return Math.sqrt(x * x + y * y);
 	};
+}
+
+
+
+/*---- Fast Fourier transform library (stripped down) ----*/
+
+/* 
+ * Free FFT and convolution (JavaScript)
+ * 
+ * Copyright (c) 2016 Project Nayuki
+ * https://www.nayuki.io/page/free-small-fft-in-multiple-languages
+ * 
+ * (MIT License)
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of
+ * this software and associated documentation files (the "Software"), to deal in
+ * the Software without restriction, including without limitation the rights to
+ * use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+ * the Software, and to permit persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * - The above copyright notice and this permission notice shall be included in
+ *   all copies or substantial portions of the Software.
+ * - The Software is provided "as is", without warranty of any kind, express or
+ *   implied, including but not limited to the warranties of merchantability,
+ *   fitness for a particular purpose and noninfringement. In no event shall the
+ *   authors or copyright holders be liable for any claim, damages or other
+ *   liability, whether in an action of contract, tort or otherwise, arising from,
+ *   out of or in connection with the Software or the use or other dealings in the
+ *   Software.
+ */
+
+
+/* 
+ * Computes the discrete Fourier transform (DFT) of the given complex vector, storing the result back into the vector.
+ * The vector's length must be a power of 2. Uses the Cooley-Tukey decimation-in-time radix-2 algorithm.
+ */
+function fastFourierTransform(real, imag) {
+    // Initialization
+    if (real.length != imag.length)
+        throw "Mismatched lengths";
+    var n = real.length;
+    if (n == 1)  // Trivial transform
+        return;
+    var levels = -1;
+    for (var i = 0; i < 32; i++) {
+        if (1 << i == n)
+            levels = i;  // Equal to log2(n)
+    }
+    if (levels == -1)
+        throw "Length is not a power of 2";
+    var cosTable = new Array(n / 2);
+    var sinTable = new Array(n / 2);
+    for (var i = 0; i < n / 2; i++) {
+        cosTable[i] = Math.cos(2 * Math.PI * i / n);
+        sinTable[i] = Math.sin(2 * Math.PI * i / n);
+    }
+    
+    // Bit-reversed addressing permutation
+    for (var i = 0; i < n; i++) {
+        var j = reverseBits(i, levels);
+        if (j > i) {
+            var temp = real[i];
+            real[i] = real[j];
+            real[j] = temp;
+            temp = imag[i];
+            imag[i] = imag[j];
+            imag[j] = temp;
+        }
+    }
+    
+    // Cooley-Tukey decimation-in-time radix-2 FFT
+    for (var size = 2; size <= n; size *= 2) {
+        var halfsize = size / 2;
+        var tablestep = n / size;
+        for (var i = 0; i < n; i += size) {
+            for (var j = i, k = 0; j < i + halfsize; j++, k += tablestep) {
+                var tpre =  real[j+halfsize] * cosTable[k] + imag[j+halfsize] * sinTable[k];
+                var tpim = -real[j+halfsize] * sinTable[k] + imag[j+halfsize] * cosTable[k];
+                real[j + halfsize] = real[j] - tpre;
+                imag[j + halfsize] = imag[j] - tpim;
+                real[j] += tpre;
+                imag[j] += tpim;
+            }
+        }
+    }
+    
+    // Returns the integer whose value is the reverse of the lowest 'bits' bits of the integer 'x'.
+    function reverseBits(x, bits) {
+        var y = 0;
+        for (var i = 0; i < bits; i++) {
+            y = (y << 1) | (x & 1);
+            x >>>= 1;
+        }
+        return y;
+    }
 }
